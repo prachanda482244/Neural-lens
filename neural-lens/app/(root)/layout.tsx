8cad31b433917d060e04f745520f15f7cf4ca125
Prachanda Rana <62231260+prachanda482244@users.noreply.github.com>
2024-02-21 21:06:15 +0545

    Complete layout Sidebar and mobile navigation

--- neural-lens/app/(root)/layout.tsx
+++ neural-lens/app/(root)/layout.tsx
@@ -1,14 +1,16 @@
+import MobileNav from "@/components/shared/MobileNav";
+import Sidebar from "@/components/shared/Sidebar";
 
 const layout = ({ children }: { children: React.ReactNode }) => {
-      return (
-            <main className="root">
-                  <div className="container">
-                        <div className="wrapper">
-                              {children}
-                        </div>
-                  </div>
-            </main>
-      )
-}
+  return (
+    <main className="root">
+      <Sidebar />
+      <MobileNav />
+      <div className="container">
+        <div className="wrapper">{children}</div>
+      </div>
+    </main>
+  );
+};
 
-export default layout
+export default layout;
--- neural-lens/app/(root)/page.tsx
+++ neural-lens/app/(root)/page.tsx
@@ -3,9 +3,7 @@ import { UserButton } from "@clerk/nextjs";
 export default function Home() {
   return (
     <div>
-      <p>
-        HOme page
-      </p>
+      <p></p>
       <UserButton afterSignOutUrl="/" />
     </div>
   );
