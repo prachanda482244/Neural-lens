221cc4e7c0fff5fcb6205f98059b9c47b33dc581
Prachanda Rana <62231260+prachanda482244@users.noreply.github.com>
2024-02-22 20:07:38 +0545

    minor changes

--- neural-lens/app/(root)/page.tsx
+++ neural-lens/app/(root)/page.tsx
@@ -1,10 +1,7 @@
-import { UserButton } from "@clerk/nextjs";
-
 export default function Home() {
   return (
     <div>
-      <p></p>
-      <UserButton afterSignOutUrl="/" />
+      <p>Home page</p>
     </div>
   );
 }
